deb21e6aae3f2707e6d29989d5ae905c75048ac6
Latisha Copeland <lcopeland001@gmail.com>
2022-06-26 22:57:45 -0500

    Q2-Q61 answered

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,6 +15,7 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName(name) {
+  console.log('personal greetinng for', name);
   return 'Hello', name;
 }
 console.log(helloName('Latisha'));
@@ -23,37 +24,55 @@ console.log(helloName('Latisha'));
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
-  // return firstNumber + secondNumber;
+function addNumbers( firstNumber, secondNumber) {
+  console.log('Add numbers tother', firstNumber, secondNumber);
+  return firstNumber + secondNumber;
 }
+console.log(addNumbers(1,3));
+console.log(addNumbers(1,7));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(num1, num2, num3 ){
+  console.log('Multiply 3 numbers', num1,num2,num3);
+  return num1*num2*num3;
 }
+
+console.log(multiplyThree(3,6,8));
+console.log(multiplyThree(2,2,2));
 
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
+  console.log('Is this number Positive?:', number);
   if ( number > 0 ){
-    return;
+    return true; 
+  } else{
+    return false;
   }
-    return;
 }
+
+console.log(isPositive(6));
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log( 'Positive', isPositive(3) );
+console.log( 'Zero', isPositive(0) );
+console.log( 'Not Positive', isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+let array=['pizza', 'sandwich', 'craker']
 
+function getLast( array ) {
+  console.log('Get last item', array);
+  let lastItem = array[array.length-1];
+  return lastItem;
 }
+
+console.log('The last iten in the array is', getLast(array));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
